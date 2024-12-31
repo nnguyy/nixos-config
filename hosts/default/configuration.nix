f79a462199de3220fbbbb6d29ca9dcc0cd555768
nnguyy <nnoahnguyenn@gmail.com>
2024-12-30 20:03:33 -0500

    polish

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -9,9 +9,19 @@
       ../../modules/nixos/locales.nix
       ../../modules/nixos/desktop.nix
       ../../modules/nixos/services.nix
-      ../../modules/nixos/packages.nix
       ../../modules/nixos/home-manager.nix
       inputs.home-manager.nixosModules.default
     ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim 
+    git
+    wget
+    waybar
+    bibata-cursors
+  ];
+
   system.stateVersion = "24.11"; # Did you read the comment?
 }
