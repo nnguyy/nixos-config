7ab995ff2740fb252cee207d79a9a531eaa1fc46
nnguyy <nnoahnguyenn@gmail.com>
2025-01-06 23:52:59 -0500

    cleanup

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -169,6 +169,7 @@
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
         "opacity 0.9 0.8, class:^(vesktop)$"
+        "opacity 0.9 0.8, class:^(Spotify)$"
       ];
     };
   };
