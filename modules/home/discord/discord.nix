78ad06cbbb5b889ce192a29c5ba91b3b0699ecc2
nnguyy <nnoahnguyenn@gmail.com>
2025-01-06 21:37:42 -0500

    added themes

--- modules/home/discord/discord.nix
+++ modules/home/discord/discord.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    (discord.override {
-    withVencord = true;
-    })
-    # webcord-vencord
+    #(discord.override {
+    #withVencord = true;
+    #})
+    webcord-vencord
   ];
   xdg.configFile."Vencord/themes/gruvbox.theme.css".source = ./gruvbox.css;
   xdg.configFile."Vencord/themes/DarkMatter.theme.css".source = ./darkmatter.css;
