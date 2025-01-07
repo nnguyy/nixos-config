7e732eb6e6f0b5e6040cef58002bdf542bd1973a
nnguyy <nnoahnguyenn@gmail.com>
2025-01-06 20:54:41 -0500

    fixes

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -23,8 +23,8 @@
         gaps_in = 3;
         gaps_out = 3;
         border_size = 2;
-        col.active_border = "rgba(ffffffaa)";
-        col.inactive_border = "rgba(595959aa)";
+        "col.active_border" = "rgba(ffffffaa)";
+        "col.inactive_border" = "rgba(595959aa)";
         resize_on_border = true;
         allow_tearing = false;
         layout = "dwindle";
@@ -123,8 +123,26 @@
         "ALT,l,resizeactive,50 0"
         "ALT,i,resizeactive,0 -50"
         "ALT,k,resizeactive,0 50"
-        "$mainMod,1,workspace,1"
+        "$mainMod,1,workspace,10"
+        "$mainMod,2,workspace,2"
+        "$mainMod,3,workspace,3"
+        "$mainMod,4,workspace,4"
+        "$mainMod,5,workspace,5"
+        "$mainMod,6,workspace,6"
+        "$mainMod,7,workspace,7"
+        "$mainMod,8,workspace,8"
+        "$mainMod,9,workspace,9"
+        "$mainMod,0,workspace,10"
         "$mainMod SHIFT,1,movetoworkspace,1"
+        "$mainMod SHIFT,2,movetoworkspace,2"
+        "$mainMod SHIFT,3,movetoworkspace,3"
+        "$mainMod SHIFT,4,movetoworkspace,4"
+        "$mainMod SHIFT,5,movetoworkspace,5"
+        "$mainMod SHIFT,6,movetoworkspace,6"
+        "$mainMod SHIFT,7,movetoworkspace,7"
+        "$mainMod SHIFT,8,movetoworkspace,8"
+        "$mainMod SHIFT,9,movetoworkspace,9"
+        "$mainMod SHIFT,0,movetoworkspace,10"
       ];
 
       bindel = [
