168abde1336c1c5f05a42824a6fa487637b76267
nnguyy <nnoahnguyenn@gmail.com>
2025-01-06 22:04:09 -0500

    fixes

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -163,7 +163,7 @@
 
       windowrule = [
         "opacity 0.9 0.8, ^(kitty)$"
-        "opacity 0.9 0.8, ^(Discord)$"
+        "opacity 0.9 0.8, title:(Discord)(.*)"
       ];
 
       windowrulev2 = [
