0c687970619645f72cc5ff9c35b8feff29d4d4d6
nnguyy <nnoahnguyenn@gmail.com>
2025-01-06 22:59:01 -0500

    fixes

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -163,12 +163,12 @@
 
       windowrule = [
         "opacity 0.9 0.8, ^(kitty)$"
-        "opacity 0.9 0.8, class:^(vesktop)$"
       ];
 
       windowrulev2 = [
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+        "opacity 0.9 0.8, class:^(vesktop)$"
       ];
     };
   };
