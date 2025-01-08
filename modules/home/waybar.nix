84b9d3abe4670c53d377733a979ecf9acd7eee3e
nnguyy <nnoahnguyenn@gmail.com>
2025-01-07 23:11:43 -0500

    added minecraft font

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       mainbar = {
-        font = "monocraft, 12"
+        font = "monocraft, 12";
         layer = "top";
         position = "top";
         modules-left = [ "hyprland/workspaces" ]; 
