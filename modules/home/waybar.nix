c2eeb88b3ae814f0a865c219b6e0efd9f97c7aaf
nnguyy <nnoahnguyenn@gmail.com>
2025-01-08 09:52:11 -0500

    edited waybar

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -15,7 +15,7 @@
           format = "{capacity} %";
         };
         "clock" = {
-          format = "{:%a %d | %b %I:%M %p}";
+          format = "{:%a %d | %b | %I:%M %p}";
           timezone = "America/New_York";
         };
       };
@@ -31,6 +31,34 @@
       window#waybar {
         background: rgba(0, 0, 0, 0.2);
         color: #ffffff;
+      }
+
+      #workspaces button {
+          padding: 5px;
+          color: #313244;
+          margin-right: 5px;
+      }
+
+      #workspaces button.active {
+          color: #a6adc8;
+      }
+
+      #workspaces button.focused {
+          color: #a6adc8;
+          background: #eba0ac;
+          border-radius: 10px;
+      }
+
+      #workspaces button.urgent {
+          color: #11111b;
+          background: #a6e3a1;
+          border-radius: 10px;
+      }
+
+      #workspaces button:hover {
+          background: #11111b;
+          color: #cdd6f4;
+          border-radius: 10px;
       }
     '';
   };
