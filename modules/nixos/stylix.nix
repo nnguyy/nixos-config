c55d17fdff2ae4d773799ff491ff0d90c68ab05b
nnguyy <nnoahnguyenn@gmail.com>
2025-01-09 13:18:13 -0500

    trying new themese with stylix

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -4,6 +4,13 @@
   stylix = {
     enable = true;
     image = ../../wallpapers/wallpaper2.png;
+
+    cursor = {
+      package = bibata-cursors;
+      name = "Bibata-Modern-Classic";
+    };
+
+
     base16Scheme = "${pkgs.base16-schemes}/share/themes/3024.yaml";
   };
 
