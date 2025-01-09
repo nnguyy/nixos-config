5c7416bded0633566ea0f2af8b336058fa28e470
nnguyy <nnoahnguyenn@gmail.com>
2025-01-09 09:59:50 -0500

    enabled stylix

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -3,12 +3,12 @@
 {
   programs.kitty = {
     enable = true;
-    font = {
-      name = "Terminess Nerd Font";
-      size = 14;
+    #font = {
+      #name = "Terminess Nerd Font";
+      #size = 14;
       #name = "Monocraft";
       #size = 12;
-    };
+    #};
     extraConfig = "
     cursor_trail 3  
     ";
