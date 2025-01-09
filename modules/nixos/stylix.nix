07b29c5fe33572ecf5336ea0816f872e77b6b04a
nnguyy <nnoahnguyenn@gmail.com>
2025-01-09 13:10:53 -0500

    trying new themese with stylix

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -4,6 +4,6 @@
   stylix.enable = true;
   stylix.image = ../../wallpapers/wallpaper2.png;
 
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gotham.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/3024.yaml";
 
 }
