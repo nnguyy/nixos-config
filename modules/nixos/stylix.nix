ef42129ddcb67f7743bee0521af96ded7186c458
nnguyy <nnoahnguyenn@gmail.com>
2025-01-10 10:50:29 -0500

    added wallpaper5

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -5,7 +5,7 @@
     enable = true;
     autoEnable = true;
     polarity = "dark";
-    image = ../../wallpapers/wallpaper4.png;
+    image = ../../wallpapers/wallpaper5.jpg;
 
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-black.yaml";
 
