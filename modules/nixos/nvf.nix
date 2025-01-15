e3415a9125e2656cead8debaa2122704abe2dd1d
nnguyy <nnoahnguyenn@gmail.com>
2025-01-15 11:16:50 -0500

    added nvf

---
+++ modules/nixos/nvf.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, ... }:
+
+{
+  programs.nvf = {
+    enable = true;
+    settings = {
+      statusline.lualine.enable = true;
+      telescope.enable = true;
+      autocomplete.nvim-cmp.enable = true;
+
+      languages = {
+        enableLSP = true;
+        enableTreesitter = true;
+
+        nix.enable = true;
+        ts.enable = true;
+        rust.enable = true;
+      };
+    };
+  };
+}
