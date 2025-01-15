f548b5375c6e2cea21438712d82fec8f05a45fff
nnguyy <nnoahnguyenn@gmail.com>
2025-01-15 12:46:40 -0500

    edited flake.nix

--- modules/nixos/nvf.nix
+++ modules/nixos/nvf.nix
@@ -3,18 +3,21 @@
 {
   programs.nvf = {
     enable = true;
-    settings.vim = {
-      statusline.lualine.enable = true;
-      telescope.enable = true;
-      autocomplete.nvim-cmp.enable = true;
+    settings = {
+      vim = {
+        statusline.lualine.enable = true;
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+        preventJunkFiles = true;
 
-      languages = {
-        enableLSP = true;
-        enableTreesitter = true;
+        languages = {
+          enableLSP = true;
+          enableTreesitter = true;
 
-        nix.enable = true;
-        ts.enable = true;
-        rust.enable = true;
+          nix.enable = true;
+          ts.enable = true;
+          rust.enable = true;
+        };
       };
     };
   };
