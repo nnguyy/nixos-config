f1007fc2be408ec6ce79b68ffa1b399be7625b4b
nnguyy <nnoahnguyenn@gmail.com>
2025-01-16 01:51:20 -0500

    adding to nvim-orgmode

--- modules/nixos/nvf.nix
+++ modules/nixos/nvf.nix
@@ -44,7 +44,7 @@
                   subtemplates = {
                     m = {
                       description = "make";
-                      template = "* %^{Project Name}\n %U\n\n* Tasks\n%?";
+                      template = "*** %^{Project Name}\n ** %U\n\n* Tasks\n%?";
                       target = "~/org/projects/%^{File Name |New Project}.org";
                     };
                     n = {
