0ff2ec5daee1dc3450aa424606ca74ec49c43db3
nnguyy <nnoahnguyenn@gmail.com>
2025-01-17 21:48:22 -0500

    add emacs nox

--- modules/home/emacs/emacs.nix
+++ modules/home/emacs/emacs.nix
@@ -6,9 +6,5 @@
     package = pkgs.emacs-nox;
   };
 
-  home.file."~/.config/emacs".source = pkgs.fetchgit {
-    url = "https://github.com/doomemacs/doomemacs";
-    rev = "main";
-  };
   home.file."~/.doom.d".source = ./.doom.d;
 }
