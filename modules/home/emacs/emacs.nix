d50c0700f64de56372456b65c871cecf3ad575c3
nnguyy <nnoahnguyenn@gmail.com>
2025-01-20 16:12:42 -0500

    manual commit

--- modules/home/emacs/emacs.nix
+++ modules/home/emacs/emacs.nix
@@ -7,11 +7,4 @@
   };
 
   home.file.".doom.d".source = ./doom;
-  home.file.".config/emacs" = {
-    source = pkgs.fetchgit {
-      url = "https://github.com/doomemacs/doomemacs";
-      rev = "2bc0524";
-      sha256 = "0a1px0igzhzl7mjl0wkp9jnb3sjggpbi2rnj0w2kga08d8frahcb";
-    };
-  };
 }
