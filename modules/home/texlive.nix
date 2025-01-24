2ee6fed558ba5f618481e98b4bac4e9c4eb7c643
nnguyy <nnoahnguyenn@gmail.com>
2025-01-24 10:32:05 -0500

    manual commit

--- modules/home/texlive.nix
+++ modules/home/texlive.nix
@@ -3,9 +3,8 @@
 {
   programs.texlive.enable = true;
 
-  programs.texlive.extraPackages = with pkgs.texlive; [
-    scheme-basic   # Or scheme-full for a complete TeX Live setup
-    latexmk        # Add other tools as needed
-  ];
+  programs.texlive.extraPackages = tpkgs: {
+    inherit (tpkgs) collection-full;
+  };
 }
 
